6093fb6abcdb1c747116e7b5eac535b7e42faddc
amy-u <amyung.lm@gmail.com>
2021-07-12 18:07:10 -0700

    created the diets controller

---
+++ app/controllers/diets_controller.rb
@@ -0,0 +1,71 @@
+class DietsController < ApplicationController
+  before_action :set_diet, only: [ :show, :edit, :update, :destroy ]
+
+  # GET /diets or /diets.json
+  def index
+    @diets = Diet.all
+    render tempate: "diets/index.html.erb", layout: "application"
+  end
+
+  # GET /diets/1 or /diets/1.json
+  def show
+  end
+
+  # GET /diets/new
+  def new
+    @diet = Diet.new
+  end
+
+  # GET /diets/1/edit
+  def edit
+  end
+
+  # POST /diets or /diets.json
+  def create
+    @diet = Diet.new(diet_params)
+
+    if @diet.save
+      flash.notice = "Diet was successfully created."
+      redirect_to @diet
+    else
+      flash.now.alert = @diet.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  # PATCH/PUT /diets/1 or /diets/1.json
+  def update
+    if @diet.update(diet_params)
+      flash.notice = "Diet was successfully updated."
+      redirect_to @diet
+    else
+      flash.now.alert = @diet.errors.full_messages.to_sentence
+      render :edit
+    end
+  end
+
+  # DELETE /diets/1 or /diets/1.json
+  def destroy
+    @diet.destroy
+    respond_to do |format|
+      format.html { redirect_to diets_url, notice: "Diet was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_diet
+      @diet = Diet.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def diet_params
+      params.require(:diet).permit(:name, :photo_url, :description_ingredients, :meal_time, :user_id)
+    end
+    def catch_not_found(e)
+      Rails.logger.debug("We had a not found exception")
+      flash.alert = e.to_s
+      redirect_to customers_path
+    end
+end
