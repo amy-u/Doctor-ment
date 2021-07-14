98003042f1281812593ff7126e4408ac854b521d
amy-u <amyung.lm@gmail.com>
2021-07-13 21:04:48 -0700

    Some validation but needs testing

--- app/models/diet.rb
+++ app/models/diet.rb
@@ -1,3 +1,12 @@
 class Diet < ApplicationRecord
     belongs_to :user
+
+  validates :name, presence: true
+  # validates :photo, allow_blank: true, format: {
+  #   with: %r{\.gif|jpg|png}i,
+  #   message: 'must be a url for gif, jpg, or png image.'
+  # }
+  validates :description, length: {maximum: 500}
+  validates :meal_time, format: { with: /\A[a-zA-Z]+\z/,
+    message: "Lunch, Dinner, Breakfast, and Snack" }
 end
