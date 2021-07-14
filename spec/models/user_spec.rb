110b00ab42d4aa19f7745eb6990fb46bf2655235
amy-u <amyung.lm@gmail.com>
2021-07-14 00:15:13 -0700

    new rspec folder with some testing for user modle

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+RSpec.describe User, type: :model do
+  subject { User.new(name: "Tom", number: "8889997777", allergic: "Mango", member_ID: "Ix98567")}
+  it "name valid" do
+    expect(subject).to be_valid
+  end
+  it "not valid without a name" do
+    subject.name=nil
+    expect(subject).to_not be_valid
+  end
+  it "not valid without a number" do 
+    subject.number=nil
+    expect(subject).to_not be_valid
+  end
+  it "not valid if number is greater than 10" do
+    subject.number = "11122244447"
+    expect(subject).to_not be_valid
+  end
+  it "not valid if number is less than 10" do
+    subject.number = "123456789"
+    expect(subject).to_not be_valid
+  end
+  it "not valid if number has other than integers" do
+    subject.number = "0jfni(*&$8"
+    expect(subject).to_not be_valid
+  end
+  it "valid if empty" do
+    subject.allergic=nil
+    expect(subject).to be_valid
+    end
+end
