0ac53212e2f44e208f5a4f2900e4c582bb1cd466
amy-u <amyung.lm@gmail.com>
2021-07-28 21:39:01 -0700

    Diet model adjusted with validtion to pass rspec

--- app/models/diet.rb
+++ app/models/diet.rb
@@ -1,12 +1,15 @@
 class Diet < ApplicationRecord
-    belongs_to :user
+    belongs_to :user, optional: true
 
-  validates :name, presence: true
+  validates :name, presence: true, format: { with: /\A[a-zA-Z\.\s]+\z/,
+    message: "only allows letters" }
   # validates :photo, allow_blank: true, format: {
   #   with: %r{\.gif|jpg|png}i,
   #   message: 'must be a url for gif, jpg, or png image.'
   # }
-  validates :description, length: {maximum: 500}
-  validates :meal_time, format: { with: /\A[a-zA-Z]+\z/,
-    message: "Lunch, Dinner, Breakfast, and Snack" }
+  validates :description, length: {maximum: 500}, allow_blank: true
+  validates :meal_time, presence: true, format: { with: /\A[a-zA-Z]+\z/}
+    # Where should I put this restriction?
+    # message: "Lunch, Dinner, Breakfast, and Snack" }
+  validates :user_id, presence: true
 end
---
+++ spec/models/diet_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+RSpec.describe Diet, type: :model do
+    subject {Diet.new(name: "Pasta", description: "Alfrado with spaghettie, milk, cheese, butter", meal_time: "Dinner", user_id: 1 )}
+    it "subject valid" do
+        expect(subject).to be_valid
+    end
+    it "not valid name is nil" do
+        subject.name = nil
+        expect(subject).to_not be_valid
+    end
+    it "not valid when name is other than letters" do
+        subject.name = "llj088435"
+        expect(subject).to_not be_valid
+    end
+    it "description should be valid when nil" do
+        subject.description = nil
+        expect(subject).to be_valid
+    end
+    it "description should be valid other than letters" do
+        subject.description  = "1/4 table spoons of butter"
+        expect(subject).to be_valid
+    end
+    it "meal time not valid when nil" do
+        subject.meal_time = nil
+        expect(subject).to_not be_valid
+    end
+    it "valid meal time string" do
+        subject.description = "Brunch"
+        expect(subject).to be_valid
+    end
+    it "not valid when user is nil" do
+        subject.user_id = nil
+        expect(subject).to_not be_valid
+    end
+end
