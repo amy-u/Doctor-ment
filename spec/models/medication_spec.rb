ce1a882affe599f511a68a9539c5b640e4bc7766
amy-u <amyung.lm@gmail.com>
2021-07-28 22:39:02 -0700

    Medication adjusted with validation for rspec

---
+++ spec/models/medication_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+RSpec.describe Medication, type: :model do
+    subject {Medication.new(name: "Vitamin C", quantity: 30, notes: "Take 2 tabs a day", user_id: 1 )}
+    it "subject valid" do 
+        expect(subject).to be_valid
+    end
+    it "name not valid except letters" do
+        subject.name = "((940**"
+        expect(subject).to_not be_valid
+    end
+    it "not valid when quantity is negative" do
+        subject.quantity = "-94"
+        expect(subject).to_not be_valid
+    end
+    it "valid when quantity is 100" do
+        subject.quantity = 100
+        expect(subject).to be_valid
+    end
+    it "valid notes when / and . is used" do
+        subject.notes = "Take 1/2 tab in the morning."
+        expect(subject).to be_valid
+    end
+    it "not valid when user id is nil" do
+        subject.user_id = nil
+        expect(subject).to_not be_valid
+    end 
+end
