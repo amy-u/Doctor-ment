0496266bde340c77b44435adbc16c0f9a6adcd17
amy-u <amyung.lm@gmail.com>
2021-07-30 01:41:58 -0700

    gem bin and configs

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-    resources 'medications'
-    resources 'doctors'
-    resources 'diets'
-
-    root 'users#index'
+  resources 'medications'
+  resources 'doctors'
+  resources 'diets'
+  resources 'users'
+  resources 'home'
+  
+  root 'home#index'
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
