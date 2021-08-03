8aae5ae0c959c143aae48e7d809ecf6ebcb29fbf
amy-u <amyung.lm@gmail.com>
2021-08-03 00:13:44 -0700

    Added Date picker in the Medication form(come back to fix*)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,14 @@
 //= require jquery
 //= require bootstrap-sprockets
+
+//= require bootstrap-datepicker
+
+//= require bootstrap.min
+//= require bootstrap-datepicker/core
+//= require bootstrap-datepicker/locales/bootstrap-datepicker.en-GB.js
+
+$(document).ready(function(){
+    $('.datepicker').datepicker({
+      format: 'mm/dd/yyyy'
+    });
+  });
